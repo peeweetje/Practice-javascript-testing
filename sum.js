7024b55bd5f169e72b9decb35589bacfcf0e2a51
Petra Vos <petravos70@gmail.com>
2019-03-04 18:29:05 +0100

    refactor: Adding tests for sum and subtract

--- sum.js
+++ sum.js
@@ -1,6 +1,19 @@
-// sum is intentionally broken so you can see errors in the tests
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
+
+let result = sum(3, 7);
+let expected = 10;
+// throws error if result is not what we expect
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
+
+result = subtract(7, 3);
+expected = 4;
+// throws error if result is not what we expect
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
 
 // these are not required , but it's just to simulate an async function
 const sumAsync = (...args) => Promise.resolve(sum(...args));
