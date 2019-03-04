5582add06851b82487c373d0c430e6b75446815c
Petra Vos <petravos70@gmail.com>
2019-03-04 19:18:11 +0100

    refactor: Add test function to quickly identify what's broken

--- sum.js
+++ sum.js
@@ -1,15 +1,19 @@
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
-let result = sum(3, 7);
-let expected = 10;
-// throws error if result is not what we expect
-expect(result).toBe(expected);
+test("sum adds number", () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  // throws error if result is not what we expect
+  expect(result).toBe(expected);
+});
 
-result = subtract(7, 3);
-expected = 4;
-// throws error if result is not what we expect
-expect(result).toBe(expected);
+test("subtract numbers", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  // throws error if result is not what we expect
+  expect(result).toBe(expected);
+});
 
 //  A function takes an actual value and
 // return an object that has functions
@@ -23,6 +27,16 @@ function expect(actual) {
       }
     }
   };
+}
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`v ${title}`);
+  } catch (error) {
+    console.error(`x ${title}`);
+    console.error(error);
+  }
 }
 
 // these are not required , but it's just to simulate an async function
