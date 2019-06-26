e33709473e62ac0a2ac8354ae47d264a0a6d1b97
peeweetje <petravos70@gmail.com>
2019-06-26 13:03:32 +0200

    Refactor: Add a new test. Replacing tests.

--- Tests/sum.test.js
+++ Tests/sum.test.js
@@ -1,4 +1,18 @@
-const { sumAsync, subtractAsync } = require("../sum");
+const { sumAsync, subtractAsync, sum, subtract } = require("../sum");
+
+test("sum adds number", () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  // throws error if result is not what we expect
+  expect(result).toBe(expected);
+});
+
+test("subtract numbers", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  // throws error if result is not what we expect
+  expect(result).toBe(expected);
+});
 
 test("sumAsync adds numbers asynchronously", async () => {
   const result = await sumAsync(3, 7);
--- sum.js
+++ sum.js
@@ -1,20 +1,6 @@
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
-test("sum adds number", () => {
-  const result = sum(3, 7);
-  const expected = 10;
-  // throws error if result is not what we expect
-  expect(result).toBe(expected);
-});
-
-test("subtract numbers", () => {
-  const result = subtract(7, 3);
-  const expected = 4;
-  // throws error if result is not what we expect
-  expect(result).toBe(expected);
-});
-
 // these are not required , but it's just to simulate an async function
 const sumAsync = (...args) => Promise.resolve(sum(...args));
 const subtractAsync = (...args) => Promise.resolve(subtract(...args));
